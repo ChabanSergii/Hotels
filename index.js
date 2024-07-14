4afdd899a09d8bb97e219df7205e64861233a486
chaban <chabchabik@gmail.com>
2024-07-14 18:30:53 +0300

    added bootstrap carousel

---
+++ index.js
@@ -0,0 +1,33 @@
+/* console.log("Hellow world!")
+
+window.addEventListener("DOMContentLoaded", function() {
+    console.log(document.querySelector("ul"))
+
+    document.querySelector(".section-placement-container").style.color = 'red',
+
+    document.querySelector(".section-placement-container").classList.add('test-class-html'),
+
+    document.querySelector(".section-placement-container").classList.toggle('test-class-html'),
+
+    document.querySelectorAll(".section-placement-container").forEach(function(domElem) {
+        console.log("!"),
+        domElem.classList.add('test-class-html')
+    }),
+
+    console.log(document.getElementsByTagName('p'))
+})
+ */
+/* 
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#burger').addEventListener('click', function() {
+        document.querySelector("#menu").classList.toggle('is-active')
+    })
+
+    document.querySelectorAll('.text').forEach(function (el) {
+        el.addEventListener('click', function(event) {
+            event.target.classList.toggle('alert')
+        })
+    })
+})
+ */
+
